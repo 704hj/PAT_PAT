4503ce79a6252a435ff4eb8ca09567e222c0fcf7
박효주 <704hj@dataeum.kr>
2025-08-22 15:55:53 +0900

    이미지 교체 및 설명서 제작 중

---
+++ app/(view)/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { forwardRef } from "react";
+
+type TLayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = forwardRef<HTMLImageElement, TLayoutProps>(
+  ({ children }, ref) => {
+    return (
+      <div className="relative w-full min-h-screen">
+        <img
+          ref={ref}
+          src="/images/bg/image2.png"
+          alt="background"
+          className="w-full h-auto min-h-screen object-cover"
+        />
+        <div className="absolute top-0 left-0 w-full h-full">{children}</div>
+      </div>
+    );
+  }
+);
+
+Layout.displayName = "Layout"; // forwardRef 쓸 때 권장
+
+export default Layout;
