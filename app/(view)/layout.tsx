8dae0611cfaebb1eef652c0f5910516c12857647
박효주 <704hj@dataeum.kr>
2025-08-22 16:53:39 +0900

    레이아웃 수정

--- app/(view)/layout.tsx
+++ app/(view)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="relative w-full min-h-screen">
       <img
         src="/images/bg/image1.png"
-        alt="background"
+        alt="bg"
         className="w-full h-auto min-h-screen object-cover"
       />
       <div className="absolute inset-0">{children}</div>
