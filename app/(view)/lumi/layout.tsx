c314426fae3e862e485cf8fd0383c34b57e7bd72
장윤영 <jyy0803@dataeum.kr>
2025-11-21 17:15:14 +0900

    main 화면 css 수정

--- app/(view)/lumi/layout.tsx
+++ app/(view)/lumi/layout.tsx
@@ -1,19 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="relative w-full min-h-screen bg-[#070F24] overflow-hidden">
-      {/* 모바일 9:16 비율 배경 이미지 */}
-      {/* <div className="absolute inset-0 w-full h-full flex justify-center items-center">
-        <img
-          src="/images/bg/sky.png"
-          alt="background"
-          className="min-w-full min-h-full w-auto h-auto object-cover"
-        />
-      </div> */}
-
-      {/* 자식 콘텐츠 */}
-      <div className="relative z-10 flex flex-col w-full h-full">
-        {children}
-      </div>
-    </div>
-  );
+  return <div>{children}</div>;
 }
